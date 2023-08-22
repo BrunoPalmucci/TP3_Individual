8891136c7994d261c5f52cbf013815887b0902d2
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2023-08-22 11:02:34 -0300

    Subida de codigo
    Subida de codigo del TPO3.

---
+++ src/main/java/a/tp3_individual/TP3_Individual.java
@@ -0,0 +1,25 @@
+
+
+/*Ejercicio 1:
+
+Crea una aplicación GUI en Java que simule un formulario de login. La interfaz debe contener los siguientes elementos:
+
+Un campo de texto (JTextField) para que el usuario pueda ingresar su mail.
+Un campo de texto (JTextField) para que el usuario pueda ingresar su password.
+Un botón (JButton) con el texto "Registrar".
+Cuando el usuario haga clic en el botón "Registrar", la aplicación debe mostrar un diálogo emergente (JOptionPane) con un mensaje de bienvenida si el mail es igual a alumno@ulp.edu.ar y el password es igual a “12345678”, caso contrario el mensaje debe decir “Usuario y/o contraseña incorrectos” .
+
+Recuerda que debes utilizar la biblioteca de componentes gráficos de Java (Swing) para crear la interfaz y gestionar los eventos del botón.
+
+Sube el Proyecto a GitHub y envía el link a través de la tarea.*/
+
+package a.tp3_individual;
+
+/**
+ *
+ * @author Bruno
+ */
+public class TP3_Individual {
+
+     //EJECUTAR LA CLASE INTERFAZLOGIN
+}
